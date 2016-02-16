aae95f534147161fe297f1058d151032ce16b44f
shenm233 <darkgenlotus@gmail.com>
2016-02-16 23:30:47 +0800

    make BaseActivity abstract

--- app/src/main/java/dg/shenm233/mmaps/ui/BaseActivity.java
+++ app/src/main/java/dg/shenm233/mmaps/ui/BaseActivity.java
@@ -6,7 +6,7 @@ import android.widget.Toast;
 
 import com.amap.api.maps.MapsInitializer;
 
-public class BaseActivity extends AppCompatActivity {
+public abstract class BaseActivity extends AppCompatActivity {
     @Override
     public void onCreate(Bundle saveInstanceStat) {
         super.onCreate(saveInstanceStat);
